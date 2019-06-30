1e4cd192b378ccdaeccd47e064bdefd63550a103
Robert <robertavellar@gmail.com>
2019-06-30 10:27:11 -0700

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const fileUpload = require('express-fileupload');
 
+const http = require('http');
+const socketIO = require('socket.io');
+const { generateMessage } = require('./utils/message');
+const { isRealString } = require('./utils/validation');
+const { Users } = require('./utils/users');
+
+// MongoDB
 const {
     DB_URL,
     PORT
@@ -50,7 +57,56 @@ app.use((err, req, res, next) => {
     console.error(err);
 });
 
+// SocketIO
+const server = http.createServer(app);
+const io = socketIO(server);
+const chatUsers = new Users();
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+  
+    socket.on('join', (params, callback) => {
+      if (!isRealString(params.username) || !isRealString(params.room)) {
+        return callback('Username and room name are required');
+      }
+  
+      socket.join(params.room);
+      chatUsers.removeUser(socket.id);
+      chatUsers.addUser(socket.id, params.username, params.room);
+  
+      io.to(params.room).emit('updateUserList', chatUsers.getUserList(params.room));
+      socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat room'));
+      socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.username} has joined.`));
+  
+      if (callback) {
+        callback();
+      }
+    });
+  
+    socket.on('createMessage', (message, callback) => {
+      const user = chatUsers.getUser(socket.id);
+  
+      if (user && isRealString(message.text)) {
+        io.to(user.room).emit('newMessage', generateMessage(user.username, message.text));
+      }
+  
+      if (callback) {
+        callback('');
+      }
+    });
+  
+    socket.on('disconnect', () => {
+      const user = chatUsers.removeUser(socket.id);
+  
+      if (user) {
+        io.to(user.room).emit('updateUserList', chatUsers.getUserList(user.room));
+        io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.username} has left`));
+      }
+    });
+  });
+
+
 // Start the server
 const port = process.env.PORT || PORT;
-app.listen(port);
+server.listen(port);
 console.log('Server listening at ' + port);
