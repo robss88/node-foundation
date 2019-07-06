0f2d11749856cf3c9b50c73f4c51867375483796
Robert <robertavellar@gmail.com>
2019-07-06 15:03:38 -0700

    move to chat to separate file

--- app.js
+++ app.js
@@ -1,113 +1,63 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const fileUpload = require('express-fileupload');
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const fileUpload = require("express-fileupload");
 
-const http = require('http');
-const socketIO = require('socket.io');
-const { generateMessage } = require('./utils/message');
-const { isRealString } = require('./utils/validation');
-const { Users } = require('./utils/users');
+const http = require("http");
 
 // MongoDB
-const {
-    DB_URL,
-    PORT
-} = require('./configuration');
+const { DB_URL, PORT } = require("./configuration");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(DB_URL);
 
 const app = express();
 app.use(helmet());
-app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : '/tmp/'
-}));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/"
+  })
+);
 
 // Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 
 // Routes
-const users = require('./routes/users')
+const users = require("./routes/users");
 
 app.use("/users", users);
 
 // Catch 404
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // Error handler function
 app.use((err, req, res, next) => {
-    const error = app.get('env') === 'development' ? err : {};
-    const status = err.status || 500;
-    // Respond to client
-    res.status(status).json({
-        error: {
-            message: error.message
-        }
-    });
-    // Respond to ourselves
-    console.error(err);
+  const error = app.get("env") === "development" ? err : {};
+  const status = err.status || 500;
+  // Respond to client
+  res.status(status).json({
+    error: {
+      message: error.message
+    }
+  });
+  // Respond to ourselves
+  console.error(err);
 });
 
 // SocketIO
 const server = http.createServer(app);
-const io = socketIO(server);
-const chatUsers = new Users();
-
-io.on('connection', (socket) => {
-    console.log('New user connected');
-  
-    socket.on('join', (params, callback) => {
-        console.log(params);
-      if (!isRealString(params.user.username) || !isRealString(params.room)) {
-        return callback('Username and room name are required');
-      }
-  
-      socket.join(params.room);
-      chatUsers.removeUser(socket.id);
-      chatUsers.addUser(socket.id, params.user, params.room);
-  
-      io.to(params.room).emit('updateUserList', chatUsers.getUserList(params.room));
-      socket.emit('newMessage', generateMessage({username : 'Admin'}, 'Welcome to the chat room'));
-      socket.broadcast.to(params.room).emit('newMessage', generateMessage({username : 'Admin'}, `${params.user.username} has joined.`));
-  
-      if (callback) {
-        callback();
-      }
-    });
-  
-    socket.on('createMessage', (message, callback) => {
-      const user = chatUsers.getUser(socket.id);
-  
-      if (user && isRealString(message.text)) {
-        io.to(user.room).emit('newMessage', generateMessage(user.userData, message.text));
-      }
-  
-      if (callback) {
-        callback('');
-      }
-    });
-  
-    socket.on('disconnect', () => {
-      const user = chatUsers.removeUser(socket.id);
-  
-      if (user) {
-        io.to(user.room).emit('updateUserList', chatUsers.getUserList(user.room));
-        io.to(user.room).emit('newMessage', generateMessage({username : 'Admin'}, `${user.userData.username} has left`));
-      }
-    });
-  });
-
+const ChatServer = require("./controllers/chat");
+ChatServer.start(server);
 
 // Start the server
 const port = process.env.PORT || PORT;
 server.listen(port);
-console.log('Server listening at ' + port);
+console.log("Server listening at " + port);
---
+++ controllers/chat.js
@@ -0,0 +1,81 @@
+const { generateMessage } = require("../utils/message");
+const { isRealString } = require("../utils/validation");
+const { Users } = require("../utils/users");
+const socketIO = require("socket.io");
+
+module.exports = {
+  start: function(server) {
+    const io = socketIO(server);
+    const chatUsers = new Users();
+
+    io.on("connection", socket => {
+      console.log("New user connected");
+
+      socket.on("join", (params, callback) => {
+        console.log(params);
+        if (!isRealString(params.user.username) || !isRealString(params.room)) {
+          return callback("Username and room name are required");
+        }
+
+        socket.join(params.room);
+        chatUsers.removeUser(socket.id);
+        chatUsers.addUser(socket.id, params.user, params.room);
+
+        io.to(params.room).emit(
+          "updateUserList",
+          chatUsers.getUserList(params.room)
+        );
+        socket.emit(
+          "newMessage",
+          generateMessage({ username: "Admin" }, "Welcome to the chat room")
+        );
+        socket.broadcast
+          .to(params.room)
+          .emit(
+            "newMessage",
+            generateMessage(
+              { username: "Admin" },
+              `${params.user.username} has joined.`
+            )
+          );
+
+        if (callback) {
+          callback();
+        }
+      });
+
+      socket.on("createMessage", (message, callback) => {
+        const user = chatUsers.getUser(socket.id);
+
+        if (user && isRealString(message.text)) {
+          io.to(user.room).emit(
+            "newMessage",
+            generateMessage(user.userData, message.text)
+          );
+        }
+
+        if (callback) {
+          callback("");
+        }
+      });
+
+      socket.on("disconnect", () => {
+        const user = chatUsers.removeUser(socket.id);
+
+        if (user) {
+          io.to(user.room).emit(
+            "updateUserList",
+            chatUsers.getUserList(user.room)
+          );
+          io.to(user.room).emit(
+            "newMessage",
+            generateMessage(
+              { username: "Admin" },
+              `${user.userData.username} has left`
+            )
+          );
+        }
+      });
+    });
+  }
+};
